2ddd8aac728be58ecec84b195d01dd81b5d3a2fe
aislan <aislan.santos@gmail.com>
2022-02-02 22:35:55 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,10 @@
+
 class Pessoa: # nome de classe segundo a pep8 usa-se CamelCase
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
